e0ac27acf9a8c837fa07457e9feb79afae77bfb1
mohammed-fandees <mohammed.fandees@gmail.com>
2025-06-15 20:37:44 +0300

    implement iphone banner

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -4,22 +4,26 @@ import Categories from "@/components/HomePage/Categories";
 import MonthProducts from "@/components/HomePage/MonthProducts";
 import Products from "@/components/HomePage/Products";
 import Featured from "@/components/HomePage/Featured";
+import { Link } from "react-router";
+import { useTranslation } from "react-i18next";
+import { ArrowRight } from "lucide-react";
 
 export default function HomePage() {
+  const { t } = useTranslation();
   return (
     <div className="">
       <div className="landing max-h-[400px] flex gap-10 mb-35">
         <Sidebar className="min-w-[250px] h-fit" />
         <div className="banner w-full  bg-black mt-10 flex justify-between items-center">
-          <div className="text text-white">
-            Iphone
+          <div className="text text-white ps-16">
+            <div className="flex gap-4 items-center mb-8">
+              <img src="/assets/apple-logo.png" alt="apple-logo"  className="w-10"/>
+              <span className="text-white mt-2">iPhone 15 Series</span>
+            </div>
+            <h2 className="text-5xl leading-[60px] tracking-[2px] mb-4">Up to 10% off Voucher</h2>
+            <Link to='' className="flex gap-3 underline">{t("common.shopNow")} <ArrowRight /></Link>
           </div>
-          <img
-            src="/products/iphone.jpg"
-            className="img-fluid rounded-top h-[80%] mx-25"
-            alt=""
-          />
-          
+          <img src="/products/iphone.jpg" className="img-fluid rounded-top h-[85%] mx-35" alt="Iphone" />
         </div>
       </div>
       <LimitedOffers />
