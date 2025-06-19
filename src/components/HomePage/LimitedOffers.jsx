0da02ccad1f5b3f5f1c6f0d34541365784dacb72
mohammed-fandees <mohammed.fandees@gmail.com>
2025-06-19 18:36:21 +0300

    filter products that has no price discount

--- src/components/HomePage/LimitedOffers.jsx
+++ src/components/HomePage/LimitedOffers.jsx
@@ -20,6 +20,7 @@ export default function LimitedOffers() {
     { label: "s", value: seconds },
   ];
 
+  const products = data.products.filter((product) => product.oldPrice);
   const [loading, setLoading] = useState(true);
 
   useEffect(() => {
@@ -29,53 +30,45 @@ export default function LimitedOffers() {
 
   return (
     <main>
-      <SectionHeader
-        title={t("home.offers.title")}
-        description={t("home.offers.description")}
-        action="navigation"
-      >
+      <SectionHeader title={t("home.offers.title")} description={t("home.offers.description")} action="navigation">
         {isEnded ? (
           <div className="text-red-600 font-bold text-xl">Event Ended</div>
         ) : (
           <div
-            className={`flex gap-4 text font-bold-center items-end absolute top-0 ${
-              isRTL ? "left-0" : "right-0"
-            } md:relative`}
+            className={`flex gap-4 text font-bold-center items-end absolute top-0 ${isRTL ? "left-0" : "right-0"
+              } md:relative`}
           >
             {loading ? [...Array(4)].map((_, index) => (
-                  <div key={index} className="flex items-end gap-1">
-                    <div className="flex flex-col items-center">
-                      <Skeleton variant="text" width={40} height={15} />
-                      <Skeleton variant="rectangular" width={32} height={38} />
-                    </div>
-                    {index < 3 && (
-                      <span className="text-[#DB4444] text-[32px] font-bold">:</span>
-                    )}
-                  </div>
-                ))
-              : timeUnits.map((unit, index) => (
-                  <div key={unit.label} className="flex items-end gap-1">
-                    <div className="flex flex-col">
-                      <span className="text-[12px] font-medium text-center">
-                        {t(`home.offers.countdown.${unit.label}`)}
+              <div key={index} className="flex items-end gap-1">
+                <div className="flex flex-col items-center">
+                  <Skeleton variant="text" width={40} height={15} />
+                  <Skeleton variant="rectangular" width={32} height={38} />
+                </div>
+                {index < 3 && (<span className="text-[#DB4444] text-[32px] font-bold">:</span>)}
+              </div>
+            )) : timeUnits.map((unit, index) => (
+                <div key={unit.label} className="flex items-end gap-1">
+                  <div className="flex flex-col">
+                    <span className="text-[12px] font-medium text-center">
+                      {t(`home.offers.countdown.${unit.label}`)}
+                    </span>
+                    <span className="countdown font-bold text-[32px]">
+                      <span style={{ "--value": unit.value }}>
+                        {unit.value}
                       </span>
-                      <span className="countdown font-bold text-[32px]">
-                        <span style={{ "--value": unit.value }}>
-                          {unit.value}
-                        </span>
-                      </span>
-                    </div>
-                    {index < timeUnits.length - 1 && (
-                      <span className="text-[#DB4444] text-[32px] font-bold">:</span>
-                    )}
+                    </span>
                   </div>
-                ))}
+                  {index < timeUnits.length - 1 && (
+                    <span className="text-[#DB4444] text-[32px] font-bold">:</span>
+                  )}
+                </div>
+              ))}
           </div>
         )}
       </SectionHeader>
       <div className="body">
         <div className="products flex gap-6 overflow-auto">
-          <ProductsSwiper products={data.products} />
+          <ProductsSwiper products={products} />
         </div>
         <div className="actions mt-10 flex justify-center">
           <Button>{t("common.viewAllProducts")}</Button>
