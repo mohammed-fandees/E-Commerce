b7ece38b3b569503db7d17e9d29b46f162e2c585
mohammed-fandees <mohammed.fandees@gmail.com>
2025-06-19 18:50:44 +0300

    add cart counter

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -7,12 +7,14 @@ import { Link } from "react-router-dom";
 import Container from "@/routes/Container";
 import { useContext } from "react";
 import { SessionContext } from "@/store/SessionContext";
+import { useCart } from "@/hooks/useCart";
 
 
 export default function Header({ navLinks }) {
   const { t } = useTranslation();
   const isRTL = getCurrentLanguage() == "ar";
   const { session } = useContext(SessionContext);
+  const { itemCount } = useCart();
 
   return (
     <header className="p-4 max-h-[70px] mt-6 border-b-1 border-[rgba(0, 0, 0, 1)] sticky top-0 bg-white z-10">
@@ -26,7 +28,12 @@ export default function Header({ navLinks }) {
               <Search />
             </div>
             <Link to="/wishlist"><Heart className="cursor-pointer" /></Link>
-            <Link to="/cart"><ShoppingCart className="cursor-pointer" /></Link>
+            <Link to="/cart" className="relative">
+              {itemCount ?
+                <p className="absolute text-sm bg-[#db4444] w-fit p-4 h-4 rounded-full text-white flex items-center justify-center -top-1 -end-1">{itemCount}</p>
+                : ""}
+              <ShoppingCart className="cursor-pointer" />
+            </Link>
             {session && <Account />}
           </div>
         </div>
