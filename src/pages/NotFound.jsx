2e199d4a1cc99675a1eb16e1937b1b4051ef3a43
mohammed-fandees <mohammed.fandees@gmail.com>
2025-06-19 15:05:52 +0300

    make 404 page responsive

--- src/pages/NotFound.jsx
+++ src/pages/NotFound.jsx
@@ -6,9 +6,9 @@ export default function NotFound() {
   return (
     <Container>
       <div className="mt-18">
-        <div className="flex flex-col items-center justify-center py-40">
-          <h1 className="text-[110px] font-medium mb-4">404 Not Found</h1>
-          <p className="text-lg text-gray-700 mb-20">Your visited page not found. You may go home page.</p>
+        <div className="flex flex-col items-center justify-center py-40 text-center">
+          <h1 className="text-6xl md:text-[110px] font-semibold mb-4"><p>404</p> Not Found</h1>
+          <p className="text-lg text-gray-700 mb-18">Your visited page not found. You may go home page.</p>
           <Link to="/">
             <Button>
               Back To Home Page
