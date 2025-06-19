2254eeab1fefa97d10c3864c8276dbc36aeb0d60
mohammed-fandees <mohammed.fandees@gmail.com>
2025-06-19 18:35:19 +0300

    add custom onClick function

--- src/components/common/Button.jsx
+++ src/components/common/Button.jsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export default function Button({ children, className, type = "" }) {
+export default function Button({ children, onClick, className, type = "" }) {
   return (
-    <button type={type} className={`cursor-pointer px-12 py-4 bg-[#DB4444] text-white rounded-sm 
+    <button onClick={onClick} type={type} className={`cursor-pointer px-12 py-4 bg-[#DB4444] text-white rounded-sm 
       transform transition-transform duration-150 ease-in-out 
       hover:brightness-110 active:scale-90 active:duration-75 ${className}
       `}>
